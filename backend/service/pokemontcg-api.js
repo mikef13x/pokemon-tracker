63db76c96c3a4f9c285dc75e4b2cabf8e1ec8069
Danny <danny.f.gomez@gmail.com>
2024-10-06 02:25:06 -0400

    create placeholder get request in pokemontcg api service

--- backend/service/pokemontcg-api.js
+++ backend/service/pokemontcg-api.js
@@ -1,11 +1,15 @@
 const router = require('express').Router();
-import pokemon from 'pokemontcgsdk'
+require('dotenv').config();
+import pokemon from 'pokemontcgsdk';
 
-pokemon.configure({apiKey: '123abc'})
 
-router.get('/', async (req, res) => {
+pokemon.configure({apiKey: process.env.POKE_API_KEY})
+
+router.get('/:pokemonName', async (req, res) => {
   try {
-    console.log('route');
+    console.log('pokemon name:', req.params.pokemonName);
+    const pokemonArray = await pokemon.card.where({q: `name:${req.params.pokemonName}`})
+    return pokemonArray
   } catch (err) {
     res.status(500).json(err);
   }
