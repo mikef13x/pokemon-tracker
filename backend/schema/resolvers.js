e8c521762f0720eb339a91a2f984ae6a7cf22abc
Danny <danny.f.gomez@gmail.com>
2024-10-06 21:19:32 -0400

    create placeholders in resolvers file

---
+++ backend/schema/resolvers.js
@@ -0,0 +1,9 @@
+const { GraphQLError } = require('graphql');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+  Query: {},
+  Mutation: {},
+};
+
+module.exports = resolvers;
