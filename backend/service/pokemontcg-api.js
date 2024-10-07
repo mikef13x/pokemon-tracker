e977611554fef01b5422f8f9ac857d06d03bcfdf
Danny <danny.f.gomez@gmail.com>
2024-10-06 23:51:18 -0400

    create example env, create poke api routes

--- backend/service/pokemontcg-api.js
+++ backend/service/pokemontcg-api.js
@@ -4,12 +4,28 @@ const pokemon = require('pokemontcgsdk');
 
 pokemon.configure({ apiKey: process.env.POKE_API_KEY });
 
-router.get('/:pokemonName', async (req, res) => {
+router.get('/pokemonName/:pokemonName', async (req, res) => {
   try {
     console.log('pokemon name:', req.params.pokemonName);
-    const pokemonArray = await pokemon.card.where({ q: `name:${req.params.pokemonName}` });
-    console.log(pokemonArray.data[0]);
-    res.json(pokemonArray.data); 
+    const result = await pokemon.card.where({ q: `name:${req.params.pokemonName}` });
+
+    const data = result.data
+    console.log("data elements:", data.length)
+    data.forEach(element => {
+      console.log(element.name, element.id)
+    });
+    res.json(data); 
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/sets/:setId', async (req, res) => {
+  try {
+    const result = await pokemon.card.all({ q: `set.id:${setId}` })
+    console.log(result)
+
+    res.json(result); 
   } catch (err) {
     res.status(500).json(err);
   }
