43bda8c887f669a54f19ba9242885ac9a8d09990
Danny <danny.f.gomez@gmail.com>
2024-10-07 00:49:38 -0400

    create addition tcg routes for searching all sets, and querying with different parameters

--- backend/service/pokemontcg-api.js
+++ backend/service/pokemontcg-api.js
@@ -4,6 +4,7 @@ const pokemon = require('pokemontcgsdk');
 
 pokemon.configure({ apiKey: process.env.POKE_API_KEY });
 
+// returns array of all cards based on specified pokemon name
 router.get('/pokemonName/:pokemonName', async (req, res) => {
   try {
     console.log('pokemon name:', req.params.pokemonName);
@@ -20,9 +21,37 @@ router.get('/pokemonName/:pokemonName', async (req, res) => {
   }
 });
 
+// returns array of all cards in specified set 
 router.get('/sets/:setId', async (req, res) => {
   try {
-    const result = await pokemon.card.all({ q: `set.id:${setId}` })
+    const result = await pokemon.card.all({ q: `set.id:${req.params.setId}` })
+    console.log(result)
+
+    res.json(result); 
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//returns array of cards based on provided query parameters
+// request ex: { "q": "set.id:sv3pt5 name:blastoise " }
+router.post('/queryCard', async (req, res) => {
+  try {
+    console.warn("attempting query")
+    const result = await pokemon.card.all(req.body)
+    console.log(result)
+
+    res.json(result); 
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+// returns array of every pokemon set
+router.get('/sets', async (req, res) => {
+  try {
+    const result = await pokemon.set.all()
     console.log(result)
 
     res.json(result); 
