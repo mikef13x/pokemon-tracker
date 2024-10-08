87e65c7f56f4db5b82b2432db1a77c027bc23da6
Danny <danny.f.gomez@gmail.com>
2024-10-08 17:04:08 -0400

    add required fields to card model, make cardId unique to prevent duplicate card entries in db

--- backend/models/card.js
+++ backend/models/card.js
@@ -1,11 +1,11 @@
 const { Schema, model } = require('mongoose');
 
 const cardSchema = new Schema({
-    name: String,
-    image: String,
-    cardId: String,
-    setId: String,
-})
+  name: { type: String, required: true },
+  image: { type: String, required: true },
+  cardId: { type: String, required: true, unique: true },
+  setId: { type: String },
+});
 
 const Card = model('Card', cardSchema);
 
