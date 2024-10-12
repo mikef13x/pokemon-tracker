3518dc305f059339212cd4fa544fe4eed6c38476
Danny <danny.f.gomez@gmail.com>
2024-10-11 23:16:42 -0400

    start frontend gql mutations

---
+++ frontend/src/utils/mutations.js
@@ -0,0 +1,27 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation login($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+      }
+    }
+  }
+`;
+
+export const CREATE_USER = gql`
+  mutation createUser($username: String!, $email: String!, $password: String!) {
+    createUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+      }
+    }
+  }
+`;
