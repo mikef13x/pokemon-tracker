30bea98b564037cb8a78ca8756b06ddddb9ef573
Danny <danny.f.gomez@gmail.com>
2024-10-13 10:17:46 -0400

    move redirect logic to page level for register.jsx

--- frontend/src/pages/register.jsx
+++ frontend/src/pages/register.jsx
@@ -1,8 +1,26 @@
 import RegisterForm from "../components/register-signin/register";
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Auth from "../utils/auth";
 
 
 export default function SignUp() {
-    return(
-        <RegisterForm/>
-    )
+    const navigate = useNavigate();
+    const [isLoggedIn, setIsLoggedIn] = useState(Auth.loggedIn());
+
+    useEffect(() => {
+        setIsLoggedIn(Auth.loggedIn());
+
+        if (isLoggedIn) {
+            navigate('/collection');
+        }
+    }, [navigate, isLoggedIn]);
+
+    if (isLoggedIn) {
+        return null;
+    }
+
+    return (
+        <RegisterForm />
+    );
 }
