6f98677e4f3e1345470c4b01fac1992fd4679901
mikef13x <mikef132044@gmail.com>
2024-10-17 04:28:10 -0400

    fixed bug where card2 button was not going over the entire card, made buttonbase width100% and removed margin right on the wrapper

--- frontend/src/components/search/searchwrapper2.jsx
+++ frontend/src/components/search/searchwrapper2.jsx
@@ -8,7 +8,7 @@ export default function SearchWrapper2({ sortedData }) {
 
     return (
         <>
-            <Box sx={{ display: 'grid', gridTemplateColumns: 'repeat(auto-fill, minmax(300px, 1fr))', gap: '20px', columnGap:'70px', marginTop: '-50px', padding:'90px', marginRight:'50px' }}>
+            <Box sx={{ display: 'grid', gridTemplateColumns: 'repeat(auto-fill, minmax(300px, 1fr))', gap: '20px', columnGap:'70px', marginTop: '-50px', padding:'90px', marginRight:'0px' }}>
                 {sortedData.map((item) => (
                     <Box key={item.id} sx={{ display: 'flex', justifyContent: 'center' }}>
                         <SearchResultCard2 title={item.title} price={formatPrice(item.price)} image={item.image} />
