edbc3a67c31102816d2cef5577c491e1c3b0f2df
mikef13x <mikef132044@gmail.com>
2024-10-17 03:23:06 -0400

    shortened height of the search result card and image respectively

--- frontend/src/components/search/searchresultcard.jsx
+++ frontend/src/components/search/searchresultcard.jsx
@@ -3,14 +3,14 @@ import { Card, CardMedia, Typography, ButtonBase, Box } from '@mui/material';
 export default function SearchResultCard({ title, price, image, onClick }) {
     return (
         <ButtonBase onClick={onClick} sx={{ display: 'block', textAlign: 'initial', width: '90%' }}>
-            <Box sx={{ width: '100%', height: '140px', backgroundColor: 'rgb(255,255,255,0.8)', display: 'flex', alignItems: 'center', marginBottom: '0px', padding: '10px', backdropFilter: 'blur(20px)' }}>
+            <Box sx={{ width: '100%', height: '120px', backgroundColor: 'rgb(255,255,255,0.8)', display: 'flex', alignItems: 'center', marginBottom: '0px', padding: '10px', backdropFilter: 'blur(20px)' }}>
                 <Card sx={{ width: '100px', height: 'auto', backgroundColor: 'transparent', boxShadow: 'none', display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
                     {image && (
                         <CardMedia
                             component="img"
                             image={image}
                             alt={title}
-                            sx={{ height: '200px', width: '100%', objectFit: 'contain' }}
+                            sx={{ height: '120px', width: '100%', objectFit: 'contain' }}
                         />
                     )}
                 </Card>
