94a979eb8a87e028f9ac5440210fb8ed43ea2209
mikef13x <mikef132044@gmail.com>
2024-11-13 15:54:05 -0500

    add width and other styling to add ellipsis to long names to prevent card height differences

--- frontend/src/components/collection/collectioncard.jsx
+++ frontend/src/components/collection/collectioncard.jsx
@@ -1,4 +1,4 @@
-import { Card, CardContent, CardMedia, Typography, ButtonBase, Paper } from '@mui/material';
+import { Card, CardContent, CardMedia, Typography, ButtonBase } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 
 export default function CollectionCard({ name, price, image, cardId }) {
@@ -6,33 +6,35 @@ export default function CollectionCard({ name, price, image, cardId }) {
     const navigate = useNavigate();
 
     const handleCardClick = () => {
-        navigate(`/market/${cardId}`, { state: {
-            from: location.pathname
-        }})
+        navigate(`/market/${cardId}`, {
+            state: {
+                from: location.pathname
+            }
+        })
     };
 
     return (
-        <ButtonBase onClick={handleCardClick} sx={{ width: '100%', display: 'block', textAlign: 'initial' }}>
-          
-                <Card sx={{ borderRadius: '10px', width: '280px', height: 'auto', margin: 'auto', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center',  background: 'linear-gradient(to bottom right, rgb(200,200,200), rgb(255,255,255))' }}>
-                    {image && (
-                        <CardMedia
-                            component="img"
-                            image={image}
-                            alt={name}
-                            sx={{ height: 'auto', width: '70%', objectFit: 'contain', margin: 'auto', marginTop: '30px', borderRadius:'10px' }}
-                        />
-                    )}
-                    <CardContent sx={{ textAlign: 'center', color: 'black', padding: '8px' }}>
-                        <Typography variant="h5" component="div">
-                            <span className='poppins-regular'>{name} #{cardId.split('-')[1]}</span>
-                        </Typography>
-                        <Typography variant="h6">
-                            <span className='poppins-regular'>{price}</span>
-                        </Typography>
-                    </CardContent>
-                </Card>
-      
+        <ButtonBase onClick={handleCardClick} style={{ width: '100%', display: 'block', textAlign: 'initial' }}>
+
+            <Card style={{ borderRadius: '10px', width: '280px', height: 'auto', margin: 'auto', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', background: 'linear-gradient(to bottom right, rgb(200,200,200), rgb(255,255,255))' }}>
+                {image && (
+                    <CardMedia
+                        component="img"
+                        image={image}
+                        alt={name}
+                        style={{ height: 'auto', width: '70%', objectFit: 'contain', margin: 'auto', marginTop: '30px', borderRadius: '10px' }}
+                    />
+                )}
+                <CardContent style={{ textAlign: 'center', color: 'black', padding: '8px' }}>
+                    <Typography variant="h5" component="div" style={{ height: '48px', width: '250px', overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap', display: 'block' }}>
+                        <span className='poppins-regular'>{name} #{cardId.split('-')[1]}</span>
+                    </Typography>
+                    <Typography variant="h6">
+                        <span className='poppins-regular'>{price}</span>
+                    </Typography>
+                </CardContent>
+            </Card>
+
         </ButtonBase>
     );
 }
