7060f853e6b5d3685ef464057d6cc72a2eb36de6
Danny <danny.f.gomez@gmail.com>
2024-11-29 22:04:02 -0500

    convert fetchPrices into a exported function, update seed command

--- backend/cron/cron.js
+++ backend/cron/cron.js
@@ -1,10 +1,10 @@
 const cron = require('node-cron');
-// const Card = require('./card'); // Adjust the path as necessary
+const fetchPrices = require('../seed/price-data/fetchPrices'); // Adjust the path as necessary
 
 // Schedule a task to run every day at midnight
-cron.schedule('0 0 * * *', () => {
+cron.schedule('0 0 * * *', async () => {
   console.log('Running a task every day at midnight');
-  // Add your task logic here
+  await fetchPrices();
 });
 
 console.log('Cron job scheduled');
